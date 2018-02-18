10ef15458d3a5d0a21baa4244fc716db7485e07b
Mark Hinojosa <markhinojosa1212@gmail.com>
2018-02-18 15:34:34 -0600

    finished

--- src/AppContainer.js
+++ src/AppContainer.js
@@ -1,10 +1,23 @@
 import App from "./App";
 import "./App.css";
 import { connect } from "react-redux";
+import {loadContacts, loadVehicles, loadComments, loadProducts } from './actions';
+
 
 function mapDispatchToProps(dispatch) {
   return {
-
+    loadContacts(){
+      dispatch(loadContacts())
+    },
+    loadVehicles(){
+      dispatch(loadVehicles())
+    },
+    loadComments(){
+      dispatch(loadComments())
+    },
+    loadProducts(){
+      dispatch(loadProducts())
+    }
   };
 }
 
